68af9bd8ed72be43e9e0e50080381964fd6d909c
bumsu01 <bumsu01@gmail.com>
2022-11-17 10:04:48 +0900

    .gradle dependency 추가 spring boot / web / db - h2 / mybatis / swagger / redis / lombok

---
+++ src/main/java/com/example/realtimerendering/RealTimeRenderingApplication.java
@@ -0,0 +1,13 @@
+package com.example.realtimerendering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeRenderingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealTimeRenderingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/realtimerendering/RealTimeRenderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.realtimerendering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeRenderingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
